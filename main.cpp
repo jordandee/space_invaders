7d7a153cdbc4c36a0d665eb11e0b5ebeb36e7e01
Jordan McConnell <pyromatical@gmail.com>
2014-09-19 17:46:00 -0600

    main reorganized: init, quit, loadImages

--- main.cpp
+++ main.cpp
@@ -4,44 +4,84 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+bool init();
+bool quit();
+
+bool loadImages();
+
+SDL_Window* gWindow;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+SDL_Surface* gScreen;
+SDL_Surface* gShipSurf;
+
 int main(int argc, char** argv)
 {
-  SDL_Window* window;
-  int w_width = 800;
-  int w_height = 800;
+  if (!init())
+    return 1;
 
-  SDL_Surface* screen;
-  SDL_Surface* ship;
+  loadImages();
+
+  SDL_BlitSurface(gShipSurf, NULL, gScreen, NULL);
+  SDL_UpdateWindowSurface(gWindow);
+
+  SDL_Delay(3000);
+
+  quit();
+  return 0;
+}
+
+// Initialize SDL2, create window, create screen surface
+bool init()
+{
+  bool success = true;
 
   if(SDL_Init(SDL_INIT_VIDEO) != 0)
   {
     printf("SDL2 Initialization failed: %s\n", SDL_GetError());
+    success = false;
   }
 
-  window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w_width, w_height, SDL_WINDOW_SHOWN);
-  if (window == NULL)
+  gWindow = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (gWindow == NULL)
   {
-    printf("SDL2 Window Creation failed: %s\n", SDL_GetError());
+    printf("Window Creation failed: %s\n", SDL_GetError());
+    success = false;
   }
 
-  screen = SDL_GetWindowSurface(window);
-
-  // bmp format needs to be 24 bit, R8G8B8
-  ship = SDL_LoadBMP("ship.bmp");
-  if (ship == NULL)
+  gScreen = SDL_GetWindowSurface(gWindow);
+  if (gScreen == NULL)
   {
-    printf("Ship BMP load failed: %s\n", SDL_GetError());
+    printf("Screen Surface Creation failed: %s\n", SDL_GetError());
+    success = false;
   }
 
-  SDL_BlitSurface(ship, NULL, screen, NULL);
+  return success;
+}
 
-  SDL_UpdateWindowSurface(window);
-
-  SDL_Delay(3000);
-
-  SDL_FreeSurface(ship);
-  SDL_DestroyWindow(window);
+// Free Surfaces, Destroy window, Shutdown SDL2
+bool quit()
+{
+  SDL_FreeSurface(gShipSurf);
+  SDL_DestroyWindow(gWindow);
   SDL_Quit();
 
-  return 0;
+  return true;
+}
+
+// Load the images needed for playing the game
+bool loadImages()
+{
+  bool success = true;
+
+  // bmp format needs to be 24 bit, R8G8B8
+  gShipSurf = SDL_LoadBMP("ship.bmp");
+  if (gShipSurf == NULL)
+  {
+    printf("Ship BMP load failed: %s\n", SDL_GetError());
+    success = false;
+  }
+
+  return success;
 }
