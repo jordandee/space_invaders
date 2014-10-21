787f41ad698717ce69318037919dc15a5c8787d3
Jordan McConnell <pyromatical@gmail.com>
2014-10-21 10:35:03 -0600

    Defense structures render as grid of rect sections

--- defense.c
+++ defense.c
@@ -34,9 +34,43 @@ void defense_logic(unsigned long dt)
 
 void defense_render()
 {
-  int i;
+  int i, x, y;
+  SDL_Rect clip, stretch_dest;
+  /*
+  clip.x = 0;
+  clip.y = 0;
+  stretch_dest.x = defense[0].rect.x;
+  stretch_dest.y = defense[0].rect.y;
+  */
+  clip.w = 4;
+  clip.h = 16;
+  stretch_dest.w = clip.w * 2;
+  stretch_dest.h = clip.h * 2;
   for (i = 0; i < 4; i++)
   {
-    SDL_BlitScaled(defensesurf1, NULL, gScreen, &defense[i].rect);
+    //SDL_BlitScaled(defensesurf1, NULL, gScreen, &defense[i].rect);
+    for (y = 0; y < 2; y++)
+    {
+      for (x = 0; x < 11; x++)
+      {
+        clip.x = x * clip.w;
+        clip.y = y * clip.h;
+        stretch_dest.x = defense[i].rect.x + (x * clip.w * 2);
+        stretch_dest.y = defense[i].rect.y + (y * clip.h * 2);
+
+        if (defense[i].state[x][y] == 1)
+        {
+          SDL_BlitScaled(defensesurf1, &clip, gScreen, &stretch_dest);
+        }
+        else if (defense[i].state[x][y] == 2)
+        {
+          SDL_BlitScaled(defensesurf2, &clip, gScreen, &stretch_dest);
+        }
+        else if (defense[i].state[x][y] == 3)
+        {
+          SDL_BlitScaled(defensesurf3, &clip, gScreen, &stretch_dest);
+        }
+      }
+    }
   }
 }
